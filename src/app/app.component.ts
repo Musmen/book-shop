e67a691920593bf9dd49f8d9d8465f800122cc04
Boss <C:\Users\\AppData\Roaming\The Bat!>
2021-02-27 23:45:40 +0300

    feat: add app-title with viewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
-import { BookModel } from 'src/app/models/book.model';
+import { APP_TITLE } from 'src/app/common/constants';
 import { mockBooksList } from 'src/app/mock-books/mock-books';
+import { BookModel } from 'src/app/models/book.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('appTitle') appTitle: ElementRef;
+
   booksList: BookModel[];
+
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit(): void {
     this.booksList = this._getMockBooksList();
+  }
+
+  ngAfterViewInit(): void {
+    this.renderer.setProperty(this.appTitle.nativeElement, 'textContent', APP_TITLE);
   }
 
   private _getMockBooksList(): BookModel[] {
