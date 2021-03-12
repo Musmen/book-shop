b8a2c9263ebdfa64f0f256f4b39c0fc33e78179c
Boss <C:\Users\\AppData\Roaming\The Bat!>
2021-03-12 13:54:26 +0300

    feat: add custom directive on-hover-colorizer

---
+++ src/app/shared/directives/on-hover-colorizer.directive.spec.ts
@@ -0,0 +1,9 @@
+import { OnHoverColorizerDirective } from './on-hover-colorizer.directive';
+
+describe('OnHoverColorizerDirective', () => {
+  it('should create an instance', () => {
+    const directive = new OnHoverColorizerDirective(undefined);
+
+    expect(directive).toBeTruthy();
+  });
+});
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { OrdersModule } from '@orders/orders.module';
 import { MaterialModule } from '@material/material.module';
+import { OnHoverColorizerDirective } from './directives/on-hover-colorizer.directive';
 
 @NgModule({
+  declarations: [OnHoverColorizerDirective],
   imports: [CommonModule, MaterialModule, OrdersModule],
-  exports: [CommonModule, MaterialModule, OrdersModule],
+  exports: [CommonModule, MaterialModule, OrdersModule, OnHoverColorizerDirective],
 })
 export class SharedModule {}
