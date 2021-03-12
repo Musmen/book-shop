e04d237b71bc09537547af792c6e45845e68eeca
Boss <C:\Users\\AppData\Roaming\The Bat!>
2021-03-12 23:24:38 +0300

    feat: add custom on-click-marker directive

--- src/app/shared/directives/on-hover-colorizer/on-hover-colorizer.directive.spec.ts
+++ src/app/shared/directives/on-hover-colorizer/on-hover-colorizer.directive.spec.ts
@@ -2,7 +2,7 @@ import { OnHoverColorizerDirective } from './on-hover-colorizer.directive';
 
 describe('OnHoverColorizerDirective', () => {
   it('should create an instance', () => {
-    const directive = new OnHoverColorizerDirective(undefined);
+    const directive = new OnHoverColorizerDirective(undefined, undefined);
 
     expect(directive).toBeTruthy();
   });
